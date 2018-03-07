ad4c68fc41ce06f282ff681bab03edeeb444082f
Vegard Hellem <vhellem@gmail.com>
2018-03-07 20:05:47 +0100

    Feat: Autocomplete, kinda works but not really...

--- frontend/tfbs/src/API.js
+++ frontend/tfbs/src/API.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Autocomplete from 'react-autocomplete';
 
 export default class TestData extends Component {
   constructor(props) {
     super(props);
-    this.state = { matrices: [] };
+    this.state = { matrices: [],
+                    selectedMatrix: ""};
     this.calculatePWMMatrix = this.calculatePWMMatrix.bind(this);
+    this.setSelectedMatrix = this.setSelectedMatrix.bind(this);
   }
 
     componentDidMount() {
@@ -19,7 +22,10 @@ export default class TestData extends Component {
         .then(data => {
           console.log('test');
           console.log(data);
-          const matrices = data;//.map(entry => entry.id);
+          const matrices = data.map(matrix => {
+              return {label: matrix}
+          });//.map(entry => entry.id);
+        console.log(matrices);
           this.setState( {matrices} );
         })
       }
@@ -39,12 +45,34 @@ export default class TestData extends Component {
               console.log(data);
             })
       }
+      setSelectedMatrix(value){
+        this.setState({
+            selectedMatrix: value,
+        })
+      }
 
     render() {
-    console.log(this.state.matrices);
+    console.log(this.state.selectedMatrix);
       return (
         <div>
-          {this.state.matrices}
+
+          <Autocomplete
+          items={this.state.matrices}
+          renderItem={(item, isHighlighted) =>
+            <div style={{ background: isHighlighted ? 'lightgray' : 'white' }}>
+              {item.label}
+            </div>
+
+  }
+           getItemValue={(item) => item.label}
+            value={this.state.selectedMatrix}
+            onChange={(e) => this.setSelectedMatrix(e.target.value)}
+            onSelect={(val) => this.setSelectedMatrix(val)}
+
+          />
+
+
+
           <button onClick={this.calculatePWMMatrix}>
             Let etter sekvens
           </button>

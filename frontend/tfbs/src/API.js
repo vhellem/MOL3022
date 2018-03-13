88c8d57b16e0e8885d8a3cb30e4f5b110d34662b
Vegard Hellem <vhellem@gmail.com>
2018-03-13 13:27:43 +0100

    Feat: Add support for multiple matrices with selection

--- frontend/tfbs/src/API.js
+++ frontend/tfbs/src/API.js
@@ -1,9 +1,10 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 import FlatButton from 'material-ui/FlatButton';
-import {Step, Stepper, StepLabel} from 'material-ui/Stepper';
-import Autocomplete from 'react-autocomplete';
+import { Step, Stepper, StepLabel } from 'material-ui/Stepper';
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
 
 export default class TestData extends Component {
   constructor(props) {
@@ -14,87 +15,88 @@ export default class TestData extends Component {
       inputValue: '',
       background: 0.25,
       stepIndex: 0,
-      finished: false
+      finished: false,
     };
-    this.calculatePWMMatrix = this
-      .calculatePWMMatrix
-      .bind(this);
-    this.setSelectedMatrix = this
-      .setSelectedMatrix
-      .bind(this);
-    this.handleChange = this
-      .handleChange
-      .bind(this);
-    this.handleBackgroundChange = this
-      .handleBackgroundChange
-      .bind(this);
+    this.calculatePWMMatrix = this.calculatePWMMatrix.bind(this);
+    this.setSelectedMatrix = this.setSelectedMatrix.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleBackgroundChange = this.handleBackgroundChange.bind(this);
   }
 
   componentDidMount() {
     fetch('http://localhost:5000/matrices', {
-        method: 'GET',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json'
-        }
-      })
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    })
       .then(results => results.json())
       .then(data => {
         console.log('test');
         console.log(data);
         const matrices = data.map(matrix => {
-          return {label: matrix};
+          return { label: matrix, value: matrix };
         }); //.map(entry => entry.id);
         console.log(matrices);
-        this.setState({matrices});
+        this.setState({ matrices });
       });
   }
   calculatePWMMatrix() {
     fetch('http://localhost:5000/calculate', {
       method: 'POST',
-      body: JSON.stringify({sequence: this.state.inputValue, matrix: 'MA0004.1', background: this.state.background}),
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json'
-        }
-      })
+      body: JSON.stringify({
+        sequence: this.state.inputValue,
+        matrix: this.state.selectedMatrix.map(matrix => {
+          return matrix.value;
+        }),
+        background: this.state.background,
+      }),
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    })
       .then(results => results.json())
       .then(data => {
         console.log(data);
       });
   }
 
-  setSelectedMatrix(value) {
-    this.setState({selectedMatrix: value});
+  setSelectedMatrix(selectedOption) {
+    this.setState({ selectedMatrix: selectedOption });
   }
 
   handleChange(e) {
-    this.setState({
-      inputValue: e.target.value
-    }, () => console.log(this.state.inputValue));
+    this.setState(
+      {
+        inputValue: e.target.value,
+      },
+      () => console.log(this.state.inputValue)
+    );
   }
 
   handleBackgroundChange(e) {
     this.setState({
-      background: parseFloat(e.target.value)
+      background: parseFloat(e.target.value),
     });
   }
 
   handleNext = () => {
-    const {stepIndex} = this.state;
+    const { stepIndex } = this.state;
     if (stepIndex < 2) {
       this.setState({
         stepIndex: stepIndex + 1,
-        finished: stepIndex >= 2
+        finished: stepIndex >= 2,
       });
     }
   };
 
   handlePrev = () => {
-    const {stepIndex} = this.state;
+    const { stepIndex } = this.state;
     if (stepIndex > 0) {
       this.setState({
-        stepIndex: stepIndex - 1
+        stepIndex: stepIndex - 1,
       });
     }
   };
@@ -102,63 +104,65 @@ export default class TestData extends Component {
   getStepContent(stepIndex) {
     switch (stepIndex) {
       case 0:
-        return <Autocomplete
-          items={this.state.matrices}
-          renderItem={(item, isHighlighted) => <div
-          style={{
-          background: isHighlighted
-            ? 'lightgray'
-            : 'white'
-        }}>
-          {item.label}
-        </div>}
-          getItemValue={item => item.label}
-          value={this.state.selectedMatrix}
-          onChange={e => this.setSelectedMatrix(e.target.value)}
-          onSelect={val => this.setSelectedMatrix(val)}/>
+        const { selectedMatrix } = this.state;
+        return (
+          <Select
+            name="select matrix"
+            value={selectedMatrix}
+            onChange={this.setSelectedMatrix}
+            options={this.state.matrices}
+            multi={true}
+          />
+        );
       case 1:
-        return <div>
+        return (
+          <div>
+            <TextField
+              style={{
+                textAlign: 'left',
+              }}
+              floatingLabelText="Skriv inn DNA-sekvens(er):"
+              floatingLabelFixed={true}
+              hintText=""
+              multiLine={true}
+              rows={1}
+              rowsMax={10}
+              value={this.state.inputValue}
+              onChange={this.handleChange}
+            />
+          </div>
+        );
+      case 2:
+        return (
           <TextField
             style={{
-            textAlign: 'left',
-          }}
-            floatingLabelText="Skriv inn DNA-sekvens(er):"
+              textAlign: 'left',
+            }}
+            floatingLabelText="Velg ønsket bakgrunnsfordeling:"
             floatingLabelFixed={true}
-            hintText=""
-            multiLine={true}
-            rows={1}
-            rowsMax={10}
-            value={this.state.inputValue}
-            onChange={this.handleChange}
-            />
-        </div>
-      case 2:
-        return <TextField
-          style={{
-          textAlign: 'left'
-        }}
-          floatingLabelText="Velg ønsket bakgrunnsfordeling:"
-          floatingLabelFixed={true}
-          value={this.state.background}
-          onChange={this.handleBackgroundChange}/>
+            value={this.state.background}
+            onChange={this.handleBackgroundChange}
+          />
+        );
       default:
         return '-';
     }
   }
 
   render() {
-    const {finished, stepIndex} = this.state;
+    const { finished, stepIndex } = this.state;
     const contentStyle = {
-      margin: '0 16px'
+      margin: '0 16px',
     };
     console.log(this.state.selectedMatrix);
     return (
       <div
         style={{
-        width: '100%',
-        maxWidth: 700,
-        margin: 'auto'
-      }}>
+          width: '100%',
+          maxWidth: 700,
+          margin: 'auto',
+        }}
+      >
         <Stepper activeStep={stepIndex}>
           <Step>
             <StepLabel>Matrise</StepLabel>
@@ -172,43 +176,44 @@ export default class TestData extends Component {
         </Stepper>
         <div style={contentStyle}>
           {finished
-            ? (
-              <p>
+            ? <p>
                 <a
                   href="#"
-                  onClick={(event) => {
-                  event.preventDefault();
-                  this.setState({stepIndex: 0, finished: false});
-                }}>
+                  onClick={event => {
+                    event.preventDefault();
+                    this.setState({ stepIndex: 0, finished: false });
+                  }}
+                >
                   Click here
                 </a>
                 to reset the example.
               </p>
-            )
-            : (
-              <div>
+            : <div>
                 {this.getStepContent(stepIndex)}
-                <div style={{
-                  marginTop: 12
-                }}>
+                <div
+                  style={{
+                    marginTop: 12,
+                  }}
+                >
                   <FlatButton
                     label="Tilbake"
                     disabled={stepIndex === 0}
                     onClick={this.handlePrev}
                     style={{
-                    marginRight: 12
-                  }}/>
+                      marginRight: 12,
+                    }}
+                  />
                   <RaisedButton
-                    label={stepIndex === 2
-                    ? 'Let etter sekvens'
-                    : 'Neste'}
+                    label={stepIndex === 2 ? 'Let etter sekvens' : 'Neste'}
                     primary={true}
-                    onClick={stepIndex === 2
-                    ? this.calculatePWMMatrix
-                    : this.handleNext}/>
+                    onClick={
+                      stepIndex === 2
+                        ? this.calculatePWMMatrix
+                        : this.handleNext
+                    }
+                  />
                 </div>
-              </div>
-            )}
+              </div>}
         </div>
       </div>
     );
